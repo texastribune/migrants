a5d8ad905800e0eebb4075e1c4f23dff3a43e619
Ryan Murphy <rdmurphy@users.noreply.github.com>
2016-09-20 12:46:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Data Visuals Kit
+
+*A work in progress. Do not use yet.*
+
+I don't know if this will be the final resting place for this, but it needs somewhere to live.
+
+## Quickstart
+
+Run these commands to create your project's folder:
+
+```sh
+git clone --depth=1 https://github.com/texastribune/idv-kit.git <name-of-project>
+cd <name-of-project> && rm -rf .git
+```
+
+Next, `npm install`.
+
+Now, get to work!
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: path.join(__dirname, '/app/scripts'),
+  entry: {
+    main: './main.js'
+  },
+  output: {
+    path: path.join(__dirname, '/dist/scripts'),
+    publicPath: '/scripts',
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json']
+  },
+  plugins: [
+    // only uncomment if you've read `docs/jquery.md` and know you need it
+    // new webpack.ProvidePlugin({
+    //   '$': 'jquery',
+    //   'jQuery': 'jquery',
+    //   'window.jQuery': 'jquery'
+    // }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'commons',
+      minChunks: 2
+    })
+  ],
+  debug: true,
+  devtool: 'cheap-module-eval-source-map'
+};
